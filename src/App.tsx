391d2018129f005786000a403be1fa3b3ed4e540
Agung Dirgantara <agungmasda29@gmail.com>
2024-01-02 20:22:39 +0700

    feat: tailwind & next-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-  
+export default function App() {
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="flex flex-col h-full bg-zinc-800 text-white">
+      <div className="flex basis-4 border-1 border-blue-400">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <b>header</b>
+          <br />
+          <br />
+          (sized to content)
         </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="flex-auto border-1 border-red-600">
+        <p>
+          <b>content</b>
+          (fills remaining space)
+        </p>
+      </div>
+      <div className="flex basis-10 border-2 border-green-400">
+        <p>
+          <b>footer</b> (fixed height)
+        </p>
+      </div>
+    </div>
   );
 }
-
-export default App;
